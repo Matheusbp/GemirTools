2f61db706148948a77d3c77ff7de9b303fecd462
Matheus BP <matheusbpamplona@outlook.com>
2020-09-26 17:25:16 -0300

    change description

--- R/crop_theusin.R
+++ R/crop_theusin.R
@@ -12,7 +12,10 @@
 #' @export
 #' @examples
 #' library("raster")
-#' #I do not have any example. Malz aí!
+#' r <- raster::raster(nrow=45, ncol=90)
+#' values(r) <- 1:ncell(r)
+#' e <- raster::extent(-160, 10, 30, 60)
+#' crop_theusin(e, r)
 
 crop_theusin <- function(dataset, shape){
 
